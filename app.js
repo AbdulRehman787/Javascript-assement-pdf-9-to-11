d96eb6bb576ad404f75de5ee863654c5fbbb4742
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2021-09-18 10:54:41 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,104 @@
+// var userInput = prompt("Enter your city name");
+// if (userInput == "Karachi") {
+//     alert("Welcome to the city of light")
+// } else if (userInput == "karachi") {
+//     alert("Welcome to the city of light")
+// } else {
+//     alert("type the Karachi")
+// }
+
+// var gender = prompt("Enter your gender");
+// if (gender == "Male") {
+//     alert("Good morning  sir")
+// } else if (gender == "Female") {
+//     alert("Good morning mam")
+// } else {
+//     alert("Enter your gender")
+// }
+
+// var signal = prompt("Enter the any one signal form")
+// if (signal == "red") {
+//     alert("just stop")
+// } else if (signal == "green") {
+//     alert("move way ")
+// } else if (signal == "orange") {
+//     alert("ready to  start")
+// }
+
+
+// var userInput = prompt("how much fuel is in your car");
+// if (userInput == "1 liter") {
+//     alert("Please refill the fuel in your car")
+// }
+// if (userInput == "0.25") {
+//     alert("Please refill the fuel in your car")
+// }
+
+// var a = prompt("enter the value");
+// if (++a === 5) {
+//     alert("given condition for value is variable")
+// }
+
+// var b = prompt("Enter the number of ", 82);
+// if (++b === "83") {
+//     alert("given condition for variable b is true");
+// }
+
+// var b = 82;
+// if (++b === 83) {
+//     alert("given condition for variable b is true");
+// }
+
+// var materialCost = 20000;
+// var labourCost = 2000;
+// var totalCost = materialCost + labourCost;
+// if (totalCost === materialCost + labourCost) {
+//     alert("the cost equal ")
+// }
+
+// if ("true") {
+//     alert("true")
+// }
+
+// if ("false") {
+//     alert("false")
+// }
+
+// if ("car " < "cat") {
+//     alert("Car is grater than cat")
+// }
+
+// var orignal_number = 2;
+// var guess_number = prompt("Guess a number between 1 and 10</br>come on! its fun");
+// if (guess_number == orignal_number) {
+//     alert("Correct Answer")
+// } else {
+//     alert("try Again")
+// }
+
+// var number = prompt("Enter the number you want to check whether it is divisible by 3 or not");
+// if (number % 3 == 0) {
+//     alert("Your number is divisible by 3")
+// } else {
+//     alert("Your number is not divisibke by 3")
+// }
+// var number = prompt("enter the number");
+// if (number % 2 == 0) {
+//     alert("Your number is even number")
+// } else {
+//     alert("your number is odd number")
+// }
+
+
+var temperature = prompt("Enter the your area temperatre");
+if (temperature < 40 == 0) {
+    alert("“It is too hot outside.”")
+} else if (temperature < 30 == 0) {
+    alert("“The Weather today is Normal.”")
+} else if (temperature < 20 == 0) {
+    alert("“Today’s Weather is cool.”")
+} else if (temperature < 10) {
+    alert("“OMG! Today’s weather is so Cool.”")
+} else {
+    alert("OMG!")
+}
